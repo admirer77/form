fc6c48b9053a9627a6526c12440ecb7ce41acc55
admirer77 <109015913+admirer77@users.noreply.github.com>
2023-07-29 18:30:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form data submitted:', formData);
+  };
+
+  return (
+    <div className="App">
+      <h1>Registration Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Last Name:
+          <input
+            type="text"
+            name="lastName"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {/* Display submitted data */}
+      <div>
+        <h2>Submitted Data</h2>
+        <p>First Name: {formData.firstName}</p>
+        <p>Last Name: {formData.lastName}</p>
+        <p>Email: {formData.email}</p>
+        <p>Password: {formData.password}</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
